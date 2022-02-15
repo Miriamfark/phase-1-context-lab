2f27977db3b720ffa330843af5f5e567473522f5
Miriam Farkas <miriamjoshua00@gmail.com>
2022-02-16 00:49:37 +0200

    commit tree 6d168e93af391c70e993476f3803314a94909bb6

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-/* Your Code Here */
+function createEmployeeRecord(employeeArr) {
+    const employeeObj = {}
+    employeeObj.firstName = employeeArr[0]
+    employeeObj.familyName = employeeArr[1]
+    employeeObj.title = employeeArr[2]
+    employeeObj.payPerHour = employeeArr[3]
+    employeeObj.timeInEvents = []
+    employeeObj.timeOutEvents = []
+
+    return employeeObj
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    const arrayOFEmployeeObjects = arrayOfArrays.map(createEmployeeRecord)
+    return arrayOFEmployeeObjects
+}
+
+function createTimeInEvent(dateStamp) {
+    const time = dateStamp.split(" ")[1]
+    this.timeInEvents.push(
+        {
+            type: "TimeIn",
+            hour: parseInt(time),
+            date: dateStamp.split(" ")[0]
+        }
+    )
+
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    const time = dateStamp.split(" ")[1]
+    this.timeOutEvents.push(
+        {
+            type: "TimeOut",
+            hour: parseInt(time),
+            date: dateStamp.split(" ")[0]
+        }
+    )
+
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp) {
+   const punchIn = this.timeInEvents.find(event => event.date === dateStamp).hour
+   const punchOut = this.timeOutEvents.find(event => event.date === dateStamp).hour
+
+   return (punchOut - punchIn)/100
+}
+
+function wagesEarnedOnDate(dateStamp) {
+   return this.payPerHour * hoursWorkedOnDate.call(this, dateStamp)
+}
+
+// function allWagesFor() {
+//     const allDates = this.timeInEvents.map(event => event.date)
+//     console.log(allDates)
+//     //wagesEarnedOnDate.call(allDates)
+// }
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +79,16 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(record => record.firstName === firstNameString)
+}
+
+// function calculatePayroll(collection) {
+//     console.log(collection)
+//    console.log(allWagesFor.call(this, collection))
+// }
+
+function calculatePayroll(collection) {
+    const payRoll = collection.map(employee => allWagesFor.call(employee))
+    return payRoll.reduce((a,b) => a+b)
+ }
